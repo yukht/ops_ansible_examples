65f18e8706ec12c3d7583999ade0e547dc048f6a
yukht <yukht_dev@bk.ru>
2023-01-27 02:41:23 +0300

    Закончил упражнение с развертыванием nginx, php-fpm на двух серверах

---
+++ nginx_php/delete_php.sh
@@ -0,0 +1,1 @@
+ansible-playbook -vv install_nginx_php.yml --vault-password-file pass_vault -t 'drop php'
--- nginx_php/init.sh
+++ nginx_php/init.sh
@@ -1,3 +1,4 @@
 #ansible-galaxy init roles/users
 #ansible-galaxy init roles/http
+#ansible-galaxy init roles/php
 
